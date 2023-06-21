3e57cbff1f7adb0dcb859b7f4c604b4e83473587
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-21 14:48:20 +0000

    Updated BaseModel.__init__()

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,13 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
-                self.key = value
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
