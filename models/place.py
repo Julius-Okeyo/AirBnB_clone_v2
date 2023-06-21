9f3a0a4b4c2f15bd43f85cb81bda4b4be11d40a9
Julius-Okeyo <jaykopiyo@gmail.com>
2023-06-21 21:47:08 +0000

    Changed back_populates to backref

--- models/place.py
+++ models/place.py
@@ -19,7 +19,7 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    reviews = relationship("Review", back_populates="place", cascade="delete")
+    reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity_assoc", viewonly=False, backref="places")
     amenity_ids = []
 
