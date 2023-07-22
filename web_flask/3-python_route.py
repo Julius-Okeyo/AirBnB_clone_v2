0f6e420551f9e144a9ddacc26c22315ec8472426
Julius-Okeyo <jaykopiyo@gmail.com>
2023-07-22 23:12:29 +0000

    Made all files executable

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,5 +22,15 @@ def c(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
