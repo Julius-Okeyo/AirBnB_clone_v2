432232a429833ff43944c0e96fe3915fbd28d9d2
Julius-Okeyo <jaykopiyo@gmail.com>
2023-07-25 07:09:49 -0700

    Pushed temporary changes

--- models/place.py
+++ models/place.py
@@ -2,7 +2,6 @@
 """ Place Module for HBNB project """
 from os import getenv
 import models
-from models.review import Review
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
@@ -10,7 +9,6 @@ from models.place_amenity import PlaceAmenityAssoc
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    from models.amenity import Amenity
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
@@ -22,20 +20,10 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity_assoc", viewonly=False, backref="places")
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
-        @property
-        def reviews(self):
-            """Get a list of all linked Reviews."""
-            review_list = []
-            for review in list(models.storage.all(Review).values()):
-                if review.place_id == self.id:
-                    review_list.append(review)
-            return review_list
-
         @property
         def amenities(self):
             """Get/set linked Amenities."""
